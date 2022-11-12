4edbeed8785c2771e28b709bcc9ad02670edabef
LucasZapico <lucas@lucaszapico.space>
2022-11-11 18:28:42 -0800

    feat: :sparkles: support global imports and reduced components into single index file

---
+++ components/index.tsx
@@ -0,0 +1,28 @@
+/**
+ * base
+ */
+
+/**
+ * atoms
+ */
+
+/**
+ * molecules
+ */
+export { default as IterationCount } from 'components/molecules/IterationCount'
+export { default as LanguageSelect } from 'components/molecules/LanguageSelect'
+
+/**
+ * organisms
+ */
+
+export { Nav, MobileNav } from 'components/organisms/Nav'
+export { default as Header } from 'components/organisms/Header'
+export { default as Footer } from 'components/organisms/Footer'
+export { default as SubFooter } from 'components/organisms/SubFooter'
+export { default as ResultsContainer } from 'components/organisms/ResultsContainer'
+
+/**
+ * Layouts
+ */
+export { default as DefaultLayout } from 'components/layout/DefaultLayout'
