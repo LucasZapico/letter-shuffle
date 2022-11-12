15ac5e5bd06c9780458274f8597928b91fc94bb3
LucasZapico <lucas@lucaszapico.space>
2022-11-11 18:28:02 -0800

    feat: :sparkles: add boilerplate components

---
+++ meta/navLinks.ts
@@ -0,0 +1,21 @@
+type MainLinksType = {
+  name: string
+  path: string
+}[]
+
+const mainLinks: MainLinksType = [
+  // {
+  //   name: 'home',
+  //   path: '/home'
+  // },
+  {
+    name: 'about',
+    path: '/about',
+  },
+  // {
+  //   name: 'contact',
+  //   path: '/contact'
+  // }
+]
+
+export { mainLinks, MainLinksType }
