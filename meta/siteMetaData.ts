a09c6cd4086a75b26b26889ff18bb9fdd4ae9f7c
LucasZapico <lucas@lucaszapico.space>
2022-11-19 05:53:28 -0800

    enhance site config with social object

--- meta/siteMetaData.ts
+++ meta/siteMetaData.ts
@@ -1,13 +1,39 @@
 const base = {
-  title: 'Blue Monkey Next TS Base',
-  description: 'A blog enabled Chakra-UI theme starter ',
+  title: 'Letter Shuffler',
+  description: 'A simple web app that shuffle around the letters of a word',
   language: 'en-us',
-  
+  copyWrite: 'lucaszapico',
+  author: '',
+  // file name of open graphy image located in public
+  openGraphImage: '',
 }
 
+/**
+ * only add the socials users name
+ *
+ * -- example --
+ * reddit: 'magicPotatos'
+ * facebook: 'Steve-McQueen'
+ * etc
+ */
+const social = {
+  tiktok: '',
+  facebook: '',
+  reddit: '',
+  linkedIn: '',
+  instagram: '',
+  twitter: '',
+  devto: '',
+  youtube: '',
+  whatsapp: '',
+  snapchat: '',
+  telegram: '',
+  pinterest: '',
+}
 
 const siteMetaData = {
   ...base,
+  ...social,
 }
 
 export default siteMetaData
