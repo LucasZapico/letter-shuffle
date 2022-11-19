6bcee2cd69b42acb5c7b4d5533e88e32fbe63972
LucasZapico <lucas@lucaszapico.space>
2022-11-19 05:53:03 -0800

    remove comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../theme/index'
+import theme from 'theme/index'
 
 export default class Document extends NextDocument {
   render() {
@@ -8,7 +8,6 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head />
         <body>
-          {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
