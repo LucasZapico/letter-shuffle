f96b4bc24579d930c9946e1d46ded99cf1d12ddc
LucasZapico <lucas@lucaszapico.space>
2022-11-19 05:40:27 -0800

    feat: :sparkles: add chakra components for MDX styling

---
+++ components/atoms/MDXComponents.tsx
@@ -0,0 +1,166 @@
+import { Heading, Text, Box, Link, useColorMode } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import NextLink from 'next/link'
+import Image from 'next/image'
+import { MagicLink } from 'components'
+
+/***
+ * Headings
+ */
+export const HeadingOneMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <Heading
+      {...rest}
+      size="2xl"
+      fontWeight="800"
+      // color={colorMode === 'dark' ? 'gray.400' : 'gray.800'}
+      colorScheme="red"
+    >
+      {children}
+    </Heading>
+  )
+}
+
+export const HeadingTwoMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <Heading
+      {...rest}
+      size="xl"
+      fontWeight="800"
+      color={colorMode === 'dark' ? 'gray.400' : 'gray.800'}
+    >
+      {children}
+    </Heading>
+  )
+}
+
+export const HeadingThreeMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <Heading
+      {...rest}
+      size="lg"
+      fontWeight="800"
+      color={colorMode === 'dark' ? 'gray.400' : 'gray.800'}
+    >
+      {children}
+    </Heading>
+  )
+}
+
+export const HeadingFourMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <Heading
+      {...rest}
+      size="md"
+      fontWeight="800"
+      color={colorMode === 'dark' ? 'gray.400' : 'gray.800'}
+    >
+      {children}
+    </Heading>
+  )
+}
+
+export const HeadingFiveMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <Heading
+      {...rest}
+      size="sm"
+      fontWeight="800"
+      color={colorMode === 'dark' ? 'gray.400' : 'gray.800'}
+    >
+      {children}
+    </Heading>
+  )
+}
+
+export const HeadingSixMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <Heading
+      {...rest}
+      size="xs"
+      fontWeight="800"
+      color={colorMode === 'dark' ? 'gray.400' : 'gray.800'}
+    >
+      {children}
+    </Heading>
+  )
+}
+
+/***
+ * Content
+ */
+
+export const BlockQuoteMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <Box
+      bg={colorMode === 'dark' ? 'gray.800' : 'gray.200'}
+      borderLeftWidth="2px"
+      paddingLeft={4}
+      paddingTop={2}
+      paddingBottom={2}
+      mb={6}
+    >
+      {children}
+    </Box>
+  )
+}
+
+export const LinkMDX = ({
+  children,
+  ...rest
+}: {
+  children: ReactNode | string
+}) => {
+  const { colorMode } = useColorMode()
+  return (
+    <MagicLink href="" {...rest} target="_blank">
+      {children}
+    </MagicLink>
+  )
+}
+
+export const ResponsiveImageMDX = ({ alt, ...rest }: { alt: string }) => (
+  <Image src="" alt={alt} layout="responsive" {...rest} />
+)
