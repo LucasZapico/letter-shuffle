25a559e2bfd5083fd8a613a96a4197bfec4f8daa
LucasZapico <lucas@lucaszapico.space>
2022-11-19 18:49:48 -0800

    feat: :sparkles: add links to other sources

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,24 +1,35 @@
-import { Flex, Heading, Box } from '@chakra-ui/react'
+import { Flex, Heading, useColorMode, Box } from '@chakra-ui/react'
 import { generate } from 'shortid'
 import { mainLinks } from 'meta/navLinks'
 import siteMetaData from 'meta/siteMetaData'
 import NextLink from 'next/link'
 import { SubFooter, MagicLink } from 'components/index'
+import Logo from 'components/assets/svg/letter-scramble-hor-dark'
 
 const Footer = () => {
+  const { colorMode } = useColorMode()
   return (
     <Box
       minH="500px"
       display={{ base: 'flex' }}
       flexDir="column"
       justifyContent="space-between"
+      m={2}
     >
       <Flex
         justifyContent="space-between"
         padding="1rem"
         flexDirection={{ base: 'column', md: 'row' }}
-      ></Flex>
-      <Flex flexDir={{ base: 'column' }}>
+      >
+        
+      </Flex>
+      <Flex flexDir={{ base: 'column' }} ml="1.5rem">
+      <Logo
+          width={{ base: '150px', md: '200px' }}
+          color={colorMode === 'dark' ? '#ffffff' : '#000000'}
+          mb={10}
+        />
+        <Box mb={6}>
         {mainLinks.map((link) => {
           return (
             <MagicLink
@@ -30,6 +41,10 @@ const Footer = () => {
             </MagicLink>
           )
         })}
+        </Box>
+        <Box mb={4}>See more projects and works at <MagicLink href="https://dev.lucaszapico.space">dev.lucaszapico.space</MagicLink></Box>
+        <Box mb={4}>Start a project at <MagicLink href="https://bluemonkeymakes.com">BlueMonkeyMakes.com</MagicLink></Box>
+
       </Flex>
       <SubFooter />
     </Box>
